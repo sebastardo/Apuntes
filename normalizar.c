e6cdff1edcfb6e7524b2a7c2b298d494ad533bce
Sebastardo <sebastardo@outlook.com>
2020-05-07 17:15:40 -0300

    Normalizar Cadenas

---
+++ normalizar.c
@@ -0,0 +1,37 @@
+#define esBlanco(x) ((x) == ' ' || (x) == '\t')
+#define aMayusc(x) ((x)>='a' && (x)<='z'?(x)-32:(x))
+#define aMinusc(x) ((x)>='A' && (x)<='Z'?(x)+32:(x))
+
+char *normalizar(char *s)
+{
+    char *ori = s;
+    char *des = s;
+
+    while(*ori)
+    {
+        while(esBlanco(*ori))
+            ori++;
+        if(*ori)
+        {
+            *des = aMayusc(*ori);
+            des++;
+            ori++;
+            while(!esBlanco(*ori) && *ori)
+            {
+                *des=aMinusc(*ori);
+                des++;
+                ori++;
+            }
+            if(esBlanco(*ori))
+            {
+                *des=aMinusc(*ori);
+                des++;
+                ori++;
+            }
+        }
+    }
+    if(des > s && (*des-1) == ' ')
+        des--;
+    *des = '\0';
+    return s;
+}
